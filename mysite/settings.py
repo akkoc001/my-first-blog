0fb364f2abf9b2bdd8e897ad20cead5f2f38d587
Akin <akon_94@hotmail.de>
2020-07-22 18:23:28 +0200

    setting zurückgesetzt

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATIC_ROOT = "/home/akkoc001/blog/static"
-STATIC_ROOT = "/home/akkoc001/blog/static/blog/base.css"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/home/akkoc001/blog')]
+#STATIC_ROOT = "/home/akkoc001/blog/static/blog/base.css"
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, '/home/akkoc001/blog')]
 #STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
